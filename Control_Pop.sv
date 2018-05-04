4ba7b592bb362edbcc8388858d344afea37c3e38
diegoreyning <ie700024@iteso.mx>
2018-05-04 00:10:08 -0500

    Practica 3 creemos que funciona

--- Control_Pop.sv
+++ Control_Pop.sv
@@ -28,45 +28,60 @@ begin
 		end
 	else begin
 		case(state)
-			IDLE: 
+		
+			IDLE: begin
+				FIFOvalue_logic <= 0;
 				if(start == 1'b1)
 					state <= START;
 				else
 					state <= IDLE;			
+			end
 			
 			START: 
 				if(start == 1'b0) begin
 					state <= POP;
-					Count_logic <= Count_logic + 1'b1;
+				//	Count_logic <= Count_logic + 1'b1;
 					FIFOvalue_logic <= FIFOTAvalue;
 				end	
 				else
 					state <= START;
 			
 			POP: 
-				if(empty == 1'b1)
+				if(empty == 1'b1) begin
 					state <= IDLE;
+					FIFOvalue_logic <= 0;
+				end
 				else
-					if(Count_logic < Matrix_length) begin
+					if(Count_logic < Matrix_length - 1) begin
+						state <= POP;
 						Count_logic <= Count_logic + 1'b1;
 						FIFOvalue_logic <= FIFOTAvalue;
-						state <= POP;
 					end
-					else 
+					else begin
+						Count_logic <= Count_logic + 1'b1;
 						state <= NOT_POP;
+						FIFOvalue_logic <= 0;
+					end
 					
 			NOT_POP:
-				if(empty == 1'b1)
+				if(empty == 1'b1) begin
 					state <= IDLE;
+					FIFOvalue_logic <= 0;
+				end
 				else
-					if(Count_logic >= Matrix_length) begin
-						if(Count_logic == 8'd7)
+					if(Count_logic >= Matrix_length - 1) begin
+						
+						if(Count_logic == 8'd7) begin
 							Count_logic <= 0;
+							state <= POP;
+							FIFOvalue_logic <= FIFOTAvalue;
+						end	
 						else begin
 							Count_logic <= Count_logic + 1'b1;
+							FIFOvalue_logic <= 0;
+							state <= NOT_POP;
 						end
-						FIFOvalue_logic <= 0;
-						state <= NOT_POP;
+						
 					end
 					else 
 						state <= POP;
@@ -86,8 +101,14 @@ begin
 pop_bit = 1'b0;
 	case(state)
 	
+	START:
+		pop_bit = 1'b0;
+	
 	POP:
 		pop_bit = 1'b1;
+		
+	NOT_POP:
+		pop_bit = 1'b0;	
 		
 	default:
 		pop_bit = 1'b0;
